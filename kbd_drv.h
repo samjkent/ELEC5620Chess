5b3d8ae05275df706f7f03fcf806212f5866a143
Sam Kent <Sam Kent>
2017-03-29 15:17:23 +0100

    re-organised

--- kbd_drv.h
+++
@@ -1,8 +0,0 @@
-volatile int * PS2_Data = (int *) 0xFF200100;
-volatile int * PS2_Control = (int *) 0xFF200104;
-
-#define KEY_UP 0x4C		// TODO: Replace placeholder mappings
-#define KEY_DOWN 0x4D
-#define KEY_LEFT 0x4F
-#define KEY_RIGHT 0x4E
-#define KEY_ENTER 0x43
